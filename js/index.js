ba03bdbe38a5a5c2b56b99f103f2f30d7bfedea4
mike-joe-webdeveloper <monmike11@gmail.com>
2022-06-13 17:12:45 +0100

    recent update

---
+++ js/index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // make it as accordion for smaller screens
+    if (window.innerWidth < 992) {
+
+        document.querySelectorAll('.sidebar .nav-link').forEach(function(element) {
+
+            element.addEventListener('click', function(e) {
+
+                let nextEl = element.nextElementSibling;
+                let parentEl = element.parentElement;
+                let allSubmenus_array = parentEl.querySelectorAll('.submenu');
+
+                if (nextEl && nextEl.classList.contains('submenu')) {
+                    e.preventDefault();
+                    if (nextEl.style.display == 'block') {
+                        nextEl.style.display = 'none';
+                    } else {
+                        nextEl.style.display = 'block';
+                    }
+                }
+            });
+        })
+    }
+    // end if innerWidth
+});
